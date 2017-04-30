c271201ab493ae40a82a8f4b3398c60d96be3ad4
Wei-Ting Tu <bluesoul@ms17.hinet.net>
2017-04-30 21:57:25 +0800

    Add input widget

--- panel.cpp
+++ panel.cpp
@@ -1,12 +1,46 @@
 #include "panel.h"
 #include <QVBoxLayout>
 #include <QPushButton>
+#include <QSpinBox>
+#include <QGroupBox>
+#include <QRadioButton>
 
 Panel::Panel(QWidget *parent) : QWidget(parent),
-    _clear(new QPushButton(tr("&Clear"), this))
+    _clear(new QPushButton(tr("&Clear"), this)),
+    _input_size(new QSpinBox(this)),
+    _generate(new QPushButton(tr("&Generate"), this))
 {
+    _input_size->setValue(20);
+    _input_size->setMinimum(0);
+    _input_size->setMaximum(100);
     QVBoxLayout *vbox = new QVBoxLayout;
     vbox->addWidget(_clear);
+    vbox->addWidget(_createInputGroup());
     vbox->addStretch(1);
     setLayout(vbox);
+}
+
+QGroupBox* Panel::_createInputGroup()
+{
+    QGroupBox *groupBox = new QGroupBox(tr("Input Setup"));
+
+    QRadioButton *radio1 = new QRadioButton(tr("&Random"));
+    QRadioButton *radio2 = new QRadioButton(tr("&Manual"));
+
+    radio1->setChecked(true);
+
+    QVBoxLayout *vbox = new QVBoxLayout;
+    vbox->addWidget(radio1);
+    vbox->addWidget(_input_size);
+    vbox->addWidget(_generate);
+    vbox->addWidget(radio2);
+    vbox->addStretch(1);
+    groupBox->setLayout(vbox);
+
+    return groupBox;
+}
+
+int Panel::get_input_size() const
+{
+    return _input_size->value();
 }
--- panel.h
+++ panel.h
@@ -4,6 +4,8 @@
 #include <QWidget>
 
 class QPushButton;
+class QSpinBox;
+class QGroupBox;
 
 class Panel : public QWidget
 {
@@ -12,13 +14,18 @@ public:
     explicit Panel(QWidget *parent = 0);
 
     QPushButton* get_clear_button() const { return _clear; }
+    int get_input_size() const;
+    QPushButton* get_generate_button() const { return _generate; }
 
 signals:
 
 public slots:
 private:
+    QGroupBox* _createInputGroup();
 
     QPushButton* _clear;
+    QSpinBox*    _input_size;
+    QPushButton* _generate;
 };
 
 #endif // PANEL_H
