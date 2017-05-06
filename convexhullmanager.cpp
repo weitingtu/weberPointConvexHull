e286904c9e511d64cf26842f8ca77c90ac1a7d3d
Wei-Ting Tu <bluesoul@ms17.hinet.net>
2017-05-07 00:20:15 +0800

    Use colinear

--- convexhullmanager.cpp
+++ convexhullmanager.cpp
@@ -294,13 +294,13 @@ QVector<Point> _andrew_monotone_chain_convex_hull(QVector<Point> P)
 
     // Build lower hull
     for (int i = 0; i < n; ++i) {
-        while (k >= 2 && cross(H[k-2], H[k-1], P[i]) <= 0) k--;
+        while (k >= 2 && cross(H[k-2], H[k-1], P[i]) < 0) k--;
         H[k++] = P[i];
     }
 
     // Build upper hull
     for (int i = n-2, t = k+1; i >= 0; i--) {
-        while (k >= t && cross(H[k-2], H[k-1], P[i]) <= 0) k--;
+        while (k >= t && cross(H[k-2], H[k-1], P[i]) < 0) k--;
         H[k++] = P[i];
     }
 
